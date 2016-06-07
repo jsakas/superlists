aa454de59f77c8b03d0941d4d62cfa4645ae8775
Jon Sakas <jon.sakas@beatport.com>
2016-06-06 19:33:58 -0600

    homepage now correctly displays items with tests

--- lists/tests.py
+++ lists/tests.py
@@ -24,6 +24,16 @@ class HomePageTest(TestCase):
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
 
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
