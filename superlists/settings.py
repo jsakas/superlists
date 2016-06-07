0ce265ff21918297e9742d69ccbb554cc076a075
Jon Sakas <jon.sakas@beatport.com>
2016-06-06 22:59:43 -0600

    add static files, refactor templates to use base template

--- superlists/settings.py
+++ superlists/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'lists', 'static'),
+]
+
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
