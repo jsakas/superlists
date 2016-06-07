6002955969ece5d1b8fbbf66796df6365af193fc
Jon Sakas <jon.sakas@beatport.com>
2016-06-06 18:28:49 -0600

    refactor checking for item in list

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith goes to homepage
@@ -33,18 +38,14 @@ class NewVisitorTest(unittest.TestCase):
         # lists "1: Buy peacock features to make a fly"
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She enters "Use peacock features to make a fly"
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Use peacock features to make a fly')
         input_box.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock features to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock features to make a fly')
 
         # The page updates again, and now shows both items on her list
 
