76c2ec08c13adbc2167ea588094e0cb7792d29ef
Jon Sakas <jon.sakas@beatport.com>
2016-06-07 22:00:30 -0600

    fix failing unit test

--- lists/tests.py
+++ lists/tests.py
@@ -20,7 +20,7 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html',
                                          request=request)
         self.assertEqual(response.content.decode(), expected_html)
-        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
 
