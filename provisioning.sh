03d41f06bb832aebf12bfe62606e4bc3ca2588a8
Jon Sakas <jon.sakas@beatport.com>
2016-06-08 23:28:28 -0600

    update requirements

--- provisioning.sh
+++ provisioning.sh
@@ -12,7 +12,7 @@ Provisioning a new site
 e.g.,, on Ubuntu:
 
     sudo apt-get update
-    sudo apt-get instal nginx git python3 python3-pip
+    sudo apt-get install nginx git python3 python3-pip
     sudo pip3 install virtualenv
 
 ## Nginx Virtual Host config
